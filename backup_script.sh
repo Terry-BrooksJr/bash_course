d5ca18f529b52be2dd3de514ce6fa393fd42eb39
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-03-23 04:26:45 -0500

    init commit

---
+++ backup_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Author: Terry Brooks
+# Date Created: 2023-03-23 04:21:55
+# Last Modified: 2023-03-23 04:22:12
+# Description: File Recovery Script
+# Usage: backup all the files in your home directory and save them all in a .tar archive
